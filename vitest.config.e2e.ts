1931db4a59ce5ad8b1df11368d374c417a1ffc77
wleklinskimateusz <wleklinskimateusz05@gmail.com>
2024-01-31 23:24:25 +0100

    chore: add SWC configuration and update test file
    chore: add vitest configuration files
    chore: update package.json scripts

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,27 @@
+import swc from 'unplugin-swc';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    root: './',
+    include: ['**/*.e2e-spec.ts'],
+    alias: {
+      src: './src',
+      test: './test',
+    },
+  },
+  resolve: {
+    alias: {
+      '@src': './src',
+      '@test': './test',
+    },
+  },
+  plugins: [
+    // This is required to build the test files with SWC
+    swc.vite({
+      // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
+      module: { type: 'es6' },
+    }),
+  ],
+});
